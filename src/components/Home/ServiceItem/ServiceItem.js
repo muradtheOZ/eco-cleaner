40e357830559693e888406529c6efc223d3cd570
Murad Ahmed <murad15-9149@diu.edu.bd>
2021-04-20 03:05:03 +0600

    price problem

--- src/components/Home/ServiceItem/ServiceItem.js
+++ src/components/Home/ServiceItem/ServiceItem.js
@@ -12,12 +12,13 @@ const ServiceItem = ({service}) => {
         <div className="col-md-4 col-sm-12">
             <div className="service-item my-4" onClick={handleBuyService}>
                 {
-                    service.image ? <img style={{width: '50px'}} src={`data:image/png;base64,${service.image.img}`}/>
+                    service.image ? <img style={{width: '100%'}} src={`data:image/png;base64,${service.image.img}`}/>
                     :
-                    <img style={{width: '50px'}} className="img-fluid mb-3" src={`https://tranquil-thicket-03462.herokuapp.com/${service.img}`} alt=""/>
+                    <img style={{width: '100%'}} className="img-fluid mb-3" src={`https://tranquil-thicket-03462.herokuapp.com/${service.img}`} alt=""/>
                 }
                 <h5 className="my-4">{service.title}</h5>
                 <p style={{color: '#707070'}}>{service.description}</p>
+                <p style={{color: '#707070'}}> Starting BDT: {service.price}</p>
             </div>
         </div>
     );
