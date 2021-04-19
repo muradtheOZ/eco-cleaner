e4b313a16f0fff8f62752f2c5a9c4669fb10859e
Murad Ahmed <murad15-9149@diu.edu.bd>
2021-04-20 01:38:09 +0600

    firebase config done

--- src/components/Home/Login/firebase.config.js
+++ src/components/Home/Login/firebase.config.js
@@ -1,9 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDC0nkztPAAa0fomW3fAqRV9BDvQeSHavo",
-  authDomain: "krrishir-kotha.firebaseapp.com",
-  projectId: "krrishir-kotha",
-  storageBucket: "krrishir-kotha.appspot.com",
-  messagingSenderId: "86438407845",
-  appId: "1:86438407845:web:8aaec6b6eb1bd90da3a74c"
+  apiKey: "AIzaSyChDXseuHj5RRYlsI58POL6x8y_7jki30A",
+  authDomain: "life-healer.firebaseapp.com",
+  projectId: "life-healer",
+  storageBucket: "life-healer.appspot.com",
+  messagingSenderId: "100721017252",
+  appId: "1:100721017252:web:f5b40fcfa14d8b14c9c131"
 };
 export default firebaseConfig;
+
