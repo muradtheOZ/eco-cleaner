4c055902ea502840f9ea9c26a71d69b544824d09
Murad Ahmed <muradahmed199771@gmail.com>
2022-01-16 23:56:54 +0600

    price decoration changed

--- src/components/Home/ServiceItem/ServiceItem.js
+++ src/components/Home/ServiceItem/ServiceItem.js
@@ -25,6 +25,7 @@ const ServiceItem = ({ service }) => {
         <div className=" col-md-4 col-sm-12">
 
             <div className="service-item my-4" onClick={handleBuyService}>
+            <p style={{ color: '#707070' }}> Starting BDT: {service.price}</p>
                 {
                     service.image ? <img style={{ width: '100%' }} src={`data:image/png;base64,${service.image.img}`}alt=""  />
                         :
@@ -32,7 +33,6 @@ const ServiceItem = ({ service }) => {
                 }
                 <h5 className="my-4">{service.title}</h5>
                 <p style={{ color: '#707070' }}>{service.description}</p>
-                <p style={{ color: '#707070' }}> Starting BDT: {service.price}</p>
             </div>
 
         </div>
